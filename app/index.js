82a4417d29a4ea1ff4a89380a2af11bbec376788
Alice Fernandes <alice.fernandes@sky.uk>
2021-01-10 21:58:13 +0000

    add support for 12h clock

--- app/index.js
+++ app/index.js
@@ -1,68 +1,79 @@
-import clock from "clock";
-import document from "document";
-import * as util from "../common/utils";
-import * as messaging from "messaging";
-import { battery } from "power";
-import { HeartRateSensor } from "heart-rate";
+import clock from 'clock';
+import document from 'document';
+import * as util from '../common/utils';
+import * as messaging from 'messaging';
+import { battery } from 'power';
+import { HeartRateSensor } from 'heart-rate';
+import { preferences } from 'user-settings';
 
 //icons by https://www.deviantart.com/ncrystal/art/Google-Now-Weather-Icons-597652261
 
 // Update the clock every minute
-clock.granularity = "minutes";
+clock.granularity = 'minutes';
 
 // Get a handle on the <text> element
-const myLabel = document.getElementById("time");
-const date_element = document.getElementById("date");
-const weather_element = document.getElementById("weather_icon");
-const battery_level_element = document.getElementById("bat_level")
-const heart_rate_element = document.getElementById("heart_rate")
+const time_element = document.getElementById('time');
+const date_element = document.getElementById('date');
+const hour_element = document.getElementById('hour');
+const weather_element = document.getElementById('weather_icon');
+const battery_level_element = document.getElementById('bat_level');
+const heart_rate_element = document.getElementById('heart_rate');
 
 const hrm = new HeartRateSensor();
 
 //Change heart rate sensor
-hrm.addEventListener("reading", () => {
-  heart_rate_element.text = `${hrm.heartRate === null ? "❤️" : hrm.heartRate + " ❤️"}`;
+hrm.addEventListener('reading', () => {
+	heart_rate_element.text = `${hrm.heartRate === null ? '❤️' : hrm.heartRate + ' ❤️'}`;
 });
 
-hrm.addEventListener("activate", () => {
-  heart_rate_element.text = `${hrm.heartRate === null ? "❤️" : hrm.heartRate + " ❤️"}`;
+hrm.addEventListener('activate', () => {
+	heart_rate_element.text = `${hrm.heartRate === null ? '❤️' : hrm.heartRate + ' ❤️'}`;
 });
 
-hrm.addEventListener("error", () => {
-  heart_rate_element.text = "❤️";
+hrm.addEventListener('error', () => {
+	heart_rate_element.text = '❤️';
 });
 
-hrm.start()
+hrm.start();
 
 clock.ontick = (evt) => {
-  let date = evt.date
-  let month = util.localeMonth(date.getMonth());
-  let weekDay = util.localeWeekDay(date.getDay());
-  let day = date.getDate();
-  let today = evt.date;
-  let hours = today.getHours();
-  let mins = util.zeroPad(today.getMinutes());
-  
-  let displayHours = util.zeroPad(hours);
+	let date = evt.date;
+	let month = util.localeMonth(date.getMonth());
+	let weekDay = util.localeWeekDay(date.getDay());
+	let day = date.getDate();
+	let today = evt.date;
+	let hours = today.getHours();
+	let mins = util.zeroPad(today.getMinutes());
 
-  
-  myLabel.text = `${displayHours}:${mins}`;
-  date_element.text = `${weekDay} ${month} ${day}`
-  battery_level_element.text = Math.floor(battery.chargeLevel) + "%";
-  
-  if(battery.chargeLevel < 10){
-    battery_level_element.style.fill = "red";
-  }
-  
-}
+	if (preferences.clockDisplay === '12h') {
+    time_element.x = (336 / 2) - 25
+    
+		if (hours > 12) {
+      hour_element.text = "PM"
+		} else {
+      hour_element.text = "AM"
+		}
+		hours = hours % 12;
+	}
 
-messaging.peerSocket.addEventListener("open", (evt) => {
-  messaging.peerSocket.send("clock_ready");
- });
+	let displayHours = util.zeroPad(hours);
 
-messaging.peerSocket.addEventListener("message", (evt) => {
-  let weatherData = JSON.parse(evt.data);
-  if(weatherData.cod === 200){
-    weather_element.href = `images/${weatherData.weather[0].icon}.png`
-  }
+	time_element.text = `${displayHours}:${mins}`;
+	date_element.text = `${weekDay} ${month} ${day}`;
+	battery_level_element.text = Math.floor(battery.chargeLevel) + '%';
+
+	if (battery.chargeLevel < 10) {
+		battery_level_element.style.fill = 'red';
+	}
+};
+
+messaging.peerSocket.addEventListener('open', (evt) => {
+	messaging.peerSocket.send('clock_ready');
+});
+
+messaging.peerSocket.addEventListener('message', (evt) => {
+	let weatherData = JSON.parse(evt.data);
+	if (weatherData.cod === 200) {
+		weather_element.href = `images/${weatherData.weather[0].icon}.png`;
+	}
 });
