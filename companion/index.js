f77316f0f8baf6079a34b0fd2a86280bae9fb7cb
Alice Fernandes <alicescfernandes@gmail.com>
2021-01-18 18:43:15 +0000

    geolocation support

--- companion/index.js
+++ companion/index.js
@@ -3,46 +3,74 @@ import * as util from '../common/utils';
 import * as messaging from 'messaging';
 import { API_KEY } from '../common/constants';
 import { settingsStorage } from 'settings';
-
-let city = undefined;
-let key = API_KEY;
-
-if (settingsStorage.getItem('city')) {
-	city = JSON.parse(settingsStorage.getItem('city')).name;
-}
-
-if (settingsStorage.getItem('key')) {
-	key = JSON.parse(settingsStorage.getItem('key')).name;
-}
-
+import { geolocation } from 'geolocation';
 const MILLISECONDS_PER_MINUTE = 1000 * 60;
+
+function getSetings() {
+	let city = undefined;
+	let key = API_KEY;
+	let useGeolocation = false;
+
+	if (settingsStorage.getItem('city')) {
+		city = JSON.parse(settingsStorage.getItem('city')).name;
+	}
+
+	if (settingsStorage.getItem('key')) {
+		key = JSON.parse(settingsStorage.getItem('key')).name;
+	}
+
+	if (settingsStorage.getItem('geolocation')) {
+		useGeolocation = settingsStorage.getItem('geolocation');
+	}
+
+	return { city, key, useGeolocation };
+}
+
+function locationSuccess(position, key) {
+	util.fetchWeatherByLatLng(key, position.coords.latitude, position.coords.longitude).then((res) => {
+		if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
+			messaging.peerSocket.send(JSON.stringify(res));
+		}
+	});
+}
+
+function locationError(city, key) {
+	let isValidString = util.validateString(city);
+
+	if (isValidString === false) return;
+
+	util.fetchWeatherBySearch(key, city).then((res) => {
+		if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
+			messaging.peerSocket.send(JSON.stringify(res));
+		}
+	});
+}
+
+function fetchWeather() {
+	let { city, key, useGeolocation } = getSetings();
+
+	if (useGeolocation === 'true') {
+		geolocation.getCurrentPosition(
+			(evt) => locationSuccess(evt, key),
+			(evt) => locationError(city, key),
+			{ enableHighAccuracy: false, maximumAge: 20 * (60 * 1000), timeout: 60 * 1000 }
+		);
+	} else {
+		let isValidString = util.validateString(city);
+
+		if (isValidString === false) return;
+
+		util.fetchWeatherBySearch(key, city).then((res) => {
+			if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
+				messaging.peerSocket.send(JSON.stringify(res));
+			}
+		});
+	}
+}
 
 function init() {
 	if (companion.permissions.granted('access_internet') && companion.permissions.granted('run_background')) {
 		companion.wakeInterval = 60 * MILLISECONDS_PER_MINUTE;
-
-		function fetchWeather() {
-			let isValidString = util.validateString(city);
-
-			if (isValidString === false) return;
-
-			util.fetchWeatherBySearch(key, city).then((res) => {
-				if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
-					messaging.peerSocket.send(JSON.stringify(res));
-				}
-			});
-		}
-
-		function updateWeather() {
-			if (settingsStorage.getItem('city')) {
-				city = JSON.parse(settingsStorage.getItem('city')).name;
-			}
-
-			if (settingsStorage.getItem('key')) {
-				key = JSON.parse(settingsStorage.getItem('key')).name;
-			}
-			fetchWeather();
-		}
 
 		if (companion.launchReasons.wokenUp) {
 			fetchWeather();
@@ -60,12 +88,12 @@ function init() {
 
 		// The companion was started due to settings changes
 		if (companion.launchReasons.settingsChanged) {
-			updateWeather();
+			fetchWeather();
 		}
 
 		// Event fires when a setting is changed
 		settingsStorage.addEventListener('change', (evt) => {
-			updateWeather();
+			fetchWeather();
 		});
 	}
 }
--- settings/index.jsx
+++ settings/index.jsx
@@ -1,30 +1,22 @@
 function HelloWorld(props) {
-    return (
-      <Page>
-        <Section
-          title={<Text bold align="center">Location settings</Text>}>
-        
-        <Text>This will be used to get the weather data</Text>
+	return (
+		<Page>
+			<Section
+				title={
+					<Text bold align="center">
+						Location settings
+					</Text>
+				}
+			>
+				<Text>This will be used to get the weather data</Text>
 
-        <TextInput
-            settingsKey="city"
-            label="City"
-            title="City"
-            type="text"
-            placeholder="Lisbon, Portugal"
-        />
-        
-         <TextInput
-            settingsKey="key"
-            label="Open Weather Map Key"
-            title="Open Weather Map Key"
-          />
-        <Text>Use your own API key to fetch the weather data</Text>
+				<TextInput settingsKey="city" label="City" title="City" type="text" placeholder="Lisbon, Portugal" />
+				<Toggle settingsKey="geolocation" label="Use Geolocation instead" />
+				<TextInput settingsKey="key" label="Open Weather Map Key" title="Open Weather Map Key" />
+				<Text>Use your own API key to fetch the weather data</Text>
+			</Section>
+		</Page>
+	);
+}
 
-        </Section>
-      </Page>
-    );
-  }
-  
-  registerSettingsPage(HelloWorld);
-  
+registerSettingsPage(HelloWorld);
